82c99abb327c2d5546e5891cca61a3cec93ec6a1
ARproyecto <108844631+ARproyecto@users.noreply.github.com>
2022-07-07 19:37:48 -0300

    cargo header

--- ar_proyecto/src/app/app.module.ts
+++ ar_proyecto/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogosComponent } from './logos/logos.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogosComponent
+    LogosComponent,
+    HeaderComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule,
