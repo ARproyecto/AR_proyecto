4eec943d3e6fb9d9c169be87b36a7a27f0a952a8
ARproyecto <108844631+ARproyecto@users.noreply.github.com>
2022-07-16 18:23:23 -0300

    proyecto

--- ar_proyecto/src/app/app.module.ts
+++ ar_proyecto/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FormsModule } from '@angular/forms';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
