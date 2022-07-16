dfc14893f7a32971981906a84bbca96c522c4c1b
ARproyecto <108844631+ARproyecto@users.noreply.github.com>
2022-07-16 13:15:23 -0300

    cargo nuevamente experiencia

--- ar_proyecto/src/app/components/experiencia/experiencia.component.spec.ts
+++ ar_proyecto/src/app/components/experiencia/experiencia.component.spec.ts
@@ -8,12 +8,10 @@ describe('ExperienciaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ExperienciaComponent]
+      declarations: [ ExperienciaComponent ]
     })
-      .compileComponents();
-  });
+    .compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ExperienciaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
